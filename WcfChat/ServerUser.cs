a805673cda76c62aae5919c2aede3a111bfff643
Yevhenii Saveliev <88836913+saveliyevyevhenii@users.noreply.github.com>
2024-01-08 18:23:56 +0200

    Create ServerUser.cs

---
+++ WcfChat/ServerUser.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.ServiceModel;
+
+namespace WcfChat
+{
+    internal class ServerUser
+    {
+        public int Id { get; set; }
+
+        public string Name { get; set; }
+
+        public OperationContext OperationCtx { get; set; }
+    }
+}
